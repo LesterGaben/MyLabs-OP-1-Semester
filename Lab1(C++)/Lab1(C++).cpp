c4520a37304e8a614e3e31673788db98196e8b10
ARTEM <ramaartem2016@gmail.com>
2021-09-17 16:55:22 +0300

    Добавьте файлы проекта.

---
+++ Lab1(C++)/Lab1(C++).cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#define PI 3.141592
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukr");
+    double d, square;
+    cout << "Довжина кола: ";
+    cin >> d;
+    square = PI * pow((d / (2 * PI)), 2);
+    cout << "Площа кола: " << square;
+    return 0;
+}
