fc64b60b13ef4a146261173cbdf008a2ef694cef
ARTEM <ramaartem2016@gmail.com>
2021-10-06 13:22:02 +0300

    first code

--- Lab1(C++)/Lab1(C++).cpp
+++ Lab1(C++)/Lab1(C++).cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Ukr");
     double d, square;
-    cout << "Довжина кола: ";
+    cout << "Довжина окола: ";
     cin >> d;
     square = PI * pow((d / (2 * PI)), 2);
-    cout << "Площа кола: " << square;
+    cout << "Площа кола: " << square <<endl;
+    system ("pause");
     return 0;
 }
---
+++ Lab2(C++)/Lab2(C++).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double x, y;
+    cout << "Введите координату x: ";
+    cin >> x;
+    cout << "Введите координату y: ";
+    cin >> y;
+    if (x == 0 && y == 0) cout << "Точка находится в начале координат" << endl;
+    else if (x == 0) cout << "Точка находится на оси Y" << endl;
+    else if (y == 0) cout << "Точка находится на оси X" << endl;
+    else if (x > 0 && y > 0) cout << "Точка находится в первом квадранте" << endl;
+    else if (x > 0 && y < 0) cout << "Точка находится в четвертом квадранте" << endl;
+    else if (x < 0 && y > 0) cout << "Точка находится во втором квадранте" << endl;
+    else cout << "Точка находится в третьем квадранте" << endl;
+    system("pause");
+    return 0;
+}
+ 
