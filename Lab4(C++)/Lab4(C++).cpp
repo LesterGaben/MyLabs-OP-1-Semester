5fa624cf9b93007673f8fc0d8190f946f70f5b36
ARTEM <ramaartem2016@gmail.com>
2021-10-27 14:07:23 +0300

    first code

---
+++ Lab4(C++)/Lab4(C++).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    double Tc, Tf;
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+    cout << endl;
+    printf("|%19s %8s|\n", "Температура", "");
+    printf("|По Цельсию|   |По Фаренгейту| \n");
+    for (Tc = 0; Tc <= n; Tc++) {
+        Tf = (9. / 5) * Tc + 32;
+        printf("|%7.2f   |   |%9.2f    | \n", Tc, Tf);
+    }
+    system("pause");
+    return 0;
+}
