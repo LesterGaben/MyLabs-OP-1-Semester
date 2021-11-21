253ce5c9084466048eed8b285c0a4a16f6f95451
ARTEM <ramaartem2016@gmail.com>
2021-11-21 18:46:56 +0200

    :)

---
+++ Lab6(C++)/Lab6(C++).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+double integral_arctg(double a, double b, double n);
+double integral_sin(double a, double b, double n);
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	double a, b, n, sum; 
+	cout << "Введите целое число a: ";
+	cin >> a;
+	cout << "Введите целое число b: ";
+	cin >> b;
+	cout << "Введите целое число, большее или равное 1: ";
+	cin >> n;
+	while (n < 1) {
+		cout << "Введите целое число, большее или равное 1: ";
+		cin >> n;
+	}
+	sum = integral_arctg(a, b, n) + integral_sin(0, M_PI, n);
+	cout << "Приблизительная сумма интегралов: " << sum << endl;
+	system("pause");
+	return 0;
+}
+
+double integral_arctg(double a, double b, double n) {
+	double h = (b - a) / n;
+	double arctg = 0;
+	for (int i = 1; i <= (n - 1); i++) {
+		arctg += pow(atan(a + i * h), 2);
+	}
+	int arctg_1;
+	arctg_1 = (pow(atan(a), 2) + pow(atan(b), 2)) / 2 + arctg;
+	double arctg_2 = arctg_1 * h;
+	return arctg_2;
+}
+
+double integral_sin(double a, double b, double n) {
+	double h = (b - a) / n;
+	double sinus = 0;
+	for (int i = 1; i <= (n - 1); i++) {
+		sinus += sin(pow(M_E, (10 * (a + i * h))));
+	}
+	int sinus_1;
+	sinus_1 = (sin(pow(M_E, (10 * a))) + sin(pow(M_E, (10 * b)))) / 2 + sinus;
+	double sinus_2 = sinus_1 * h;
+	return sinus_2;
+}
---
+++ Lab6(Python)/Lab6_Python_.py
@@ -0,0 +1,34 @@
+import math
+
+
+def integral_arctg(a, b, n):
+    h = (b - a) / n
+    arctg = 0
+    for i in range(1, n, 1):
+        arctg += pow(math.atan(a + i * h), 2)
+    arctg_1 = (pow(math.atan(a), 2) + pow(math.atan(b), 2)) / 2 + arctg
+    arctg_1 = int(arctg_1)
+    arctg_2 = arctg_1 * h
+    return arctg_2
+
+
+def integral_sin(a, b, n):
+    h = (b - a) / n
+    sin = 0
+    for i in range(1, n, 1):
+        sin += math.sin(pow(math.e,(10 * (a + i * h))))
+    sin_1 = (math.sin(pow(math.e, (10 * a))) + math.sin(pow(math.e, (10 * b)))) / 2 + sin
+    sin_1 = int(sin_1)
+    sin_2 = sin_1 * h
+    return sin_2
+
+
+a = int(input("Введите целое число a: "))
+b = int(input("Введите целое число b: "))
+n = int(input("Введите целое число, большее или равное 1: "))
+
+while (n < 1):
+    n = int(input("Введите целое число, большее или равное 1: "))
+
+sum = integral_arctg(a, b, n) + integral_sin(0, math.pi, n)
+print("Приблизительная сумма интегралов: " + str(sum))
